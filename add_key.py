4a719329b765c02e8b5b8c331e82e6d603d19b2b
akbasal <alperenakbas42@hotmail.com>
2025-10-21 18:02:51 +0300

    added keys to transfers.csv and player_valuations.csv

---
+++ add_key.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv("./datas/transfers.csv")
+df['transfer_id'] = df.index + 1
+df.insert(loc=0, column="transfer_id", value=df.pop('transfer_id'))
+df.to_csv(index=False, path_or_buf="./datas/transfers1.csv")
+
+
+df = pd.read_csv("./datas/player_valuations.csv")
+df['valuation_id'] = df.index + 1
+df.insert(loc=0, column="valuation_id", value=df.pop('valuation_id'))
+df.to_csv(index=False, path_or_buf="./datas/player_valuations.csv")
