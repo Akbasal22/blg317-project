0df65795265cefbf890204e5b91bb13c70e75c82
akbasal <alperenakbas42@hotmail.com>
2025-10-21 18:06:16 +0300

    minor fix to player_valuations.csv

--- add_key.py
+++ add_key.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv("./datas/transfers.csv")
-df['transfer_id'] = df.index + 1
-df.insert(loc=0, column="transfer_id", value=df.pop('transfer_id'))
-df.to_csv(index=False, path_or_buf="./datas/transfers1.csv")
+# df = pd.read_csv("./datas/transfers.csv")
+# df['transfer_id'] = df.index + 1
+# df.insert(loc=0, column="transfer_id", value=df.pop('transfer_id'))
+# df.to_csv(index=False, path_or_buf="./datas/transfers1.csv")
 
 
 df = pd.read_csv("./datas/player_valuations.csv")
-df['valuation_id'] = df.index + 1
-df.insert(loc=0, column="valuation_id", value=df.pop('valuation_id'))
+# df['valuation_id'] = df.index + 1
+df = df.drop(columns=['transfer_id'])
+# df.insert(loc=0, column="valuation_id", value=df.pop('valuation_id'))
 df.to_csv(index=False, path_or_buf="./datas/player_valuations.csv")
