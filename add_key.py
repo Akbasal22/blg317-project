5058310844d17944e53f8bc4023336edd7080e53
akbasal <alperenakbas42@hotmail.com>
2025-10-22 11:26:49 +0300

    fixed club_games primary keys

--- add_key.py
+++ add_key.py
@@ -6,8 +6,12 @@ import pandas as pd
 # df.to_csv(index=False, path_or_buf="./datas/transfers1.csv")
 
 
-df = pd.read_csv("./datas/player_valuations.csv")
-# df['valuation_id'] = df.index + 1
-df = df.drop(columns=['transfer_id'])
-# df.insert(loc=0, column="valuation_id", value=df.pop('valuation_id'))
-df.to_csv(index=False, path_or_buf="./datas/player_valuations.csv")
+# df = pd.read_csv("./datas/player_valuations.csv")
+# # df['valuation_id'] = df.index + 1
+# df = df.drop(columns=['transfer_id'])
+# # df.insert(loc=0, column="valuation_id", value=df.pop('valuation_id'))
+# df.to_csv(index=False, path_or_buf="./datas/player_valuations.csv")
+
+df = pd.read_csv("./datas/club_games.csv")
+df['game_id'] = (df['game_id'].astype(str) + (df['hosting'] == 'Home').astype(int).astype(str)).astype(int)
+df.to_csv(index=False, path_or_buf="./datas/club_games1.csv")
